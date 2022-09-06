4a35e424f90b393463145419bedb6fc031f05b3f
lfsalasnu <105181566+lfsalasnu@users.noreply.github.com>
2022-09-05 19:06:12 -0500

    dos

---
+++ Calculadoras/G27/Calc_G27.js
@@ -0,0 +1,32 @@
+let pantalla=document.getElementById('pantalla');
+
+function funcion_0(){
+    pantalla.innerHTML+='0';  
+}
+function funcion_1(){
+    pantalla.innerHTML+='1';  
+}
+function funcion_2(){
+    pantalla.innerHTML+='2';  
+}
+function funcion_3(){
+    pantalla.innerHTML+='3';  
+}
+function funcion_4(){
+    pantalla.innerHTML+='4';  
+}
+function funcion_5(){
+    pantalla.innerHTML+='5';  
+}
+function funcion_6(){
+    pantalla.innerHTML+='6';  
+}
+function funcion_7(){
+    pantalla.innerHTML+='7';  
+}
+function funcion_8(){
+    pantalla.innerHTML+='8';  
+}
+function funcion_9(){
+    pantalla.innerHTML+='9';  
+}
