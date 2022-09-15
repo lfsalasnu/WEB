983452f7ae6dbfa9fe0ad0727f7cef8374419dd2
lfsalasnu <105181566+lfsalasnu@users.noreply.github.com>
2022-09-14 20:01:29 -0500

    JPA2

--- Sprint Boot/G27/codigo/src/main/java/g27/codigo/CodigoApplication.java
+++ Sprint Boot/G27/codigo/src/main/java/g27/codigo/CodigoApplication.java
@@ -1,22 +1,30 @@
 package g27.codigo;
 
+//import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import g27.codigo.Model.Entidad1;
+import g27.codigo.Repositorios.Entidad1_repo;
 
 @SpringBootApplication
 @RestController
 public class CodigoApplication {
 
+	@Autowired
+	private Entidad1_repo rp;
 	public static void main(String[] args) {
 		SpringApplication.run(CodigoApplication.class, args);
 	}
 
 	@GetMapping("/hola")
-	String hola(@RequestParam(value = "name", defaultValue = "Mundo") String name){
-		return ("Hola " + name );
+	public Optional<Entidad1> hola(){
+		return rp.findById(1);
 	}
 
 }
